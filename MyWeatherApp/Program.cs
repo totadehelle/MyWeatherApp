cc8bf120c428b69489ed9d485172a30de9a43375
totadehelle <alt3rnata@gmail.com>
2018-11-11 05:19:53 +0300

    11/11/2018-1 Adds downloading and decompressing of source JSON-file with location ID's (from openweathermap.org)

--- MyWeatherApp/Program.cs
+++ MyWeatherApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Linq;
-using SQLitePCL;
+using MyWeatherApp.LocationsRepository;
 
 namespace MyWeatherApp
 {
@@ -13,36 +13,37 @@ namespace MyWeatherApp
                 client.Database.EnsureCreated();
             }
             
+            CitiesDbBuilder builder = new CitiesDbBuilder(new LocationsContext());
             
-            
-            LocationIdListBuilder builder = new LocationIdListBuilder(new LocationsContext());
+            builder.DecompressSourceFile(builder.DownloadSourceFile());
+            return;
 
             if (builder._context.Cities.Count() == 0)
             {
-                builder.GetLocationIdDb();
+                builder.MakeCitiesDbFromJson();
             }
             
 
             //For testing only!
             string locationId;
             
-            var cityId = from city in builder._context.Cities
+            var citiesFound = from city in builder._context.Cities
                 where city.Name == "Novinki"
                 select city;
 
-            if (cityId.Count() == 0)
+            if (citiesFound.Count() == 0)
             {
-                Console.WriteLine("There is no such city in our database, please check if the spelling is correct.");
+                Console.WriteLine("The city is not found.");
                 return;
             }
                 
-            if (cityId.Count() > 1)
+            if (citiesFound.Count() > 1)
             {
-                foreach (var city in cityId)
+                foreach (var city in citiesFound)
                 {
                     //если при запуске приложения база уже существует, Coord получаемых объектов = null и здесь приложение падает. Если база создается после запуска, то все ок.
                     Console.WriteLine(
-                        $"ID: {city.Id} \n City: {city.Name} \n Country: {city.Country} \n Coordinates: \n longitude: {city.Coord.lon}, \n latitude: {city.Coord.lat}");
+                        $"ID: {city.Id} \n City: {city.Name} \n Country: {city.Country} \n Coordinates: \n longitude: {city.Coord.lon}, \n latitude: {city.Coord.lat} \n");
                 }
 
                 Console.WriteLine("Please select your city and print its id:");
@@ -50,7 +51,7 @@ namespace MyWeatherApp
                 return;
             }
 
-            locationId = cityId.First().Id.ToString();
+            locationId = citiesFound.First().Id.ToString();
             
             
             
